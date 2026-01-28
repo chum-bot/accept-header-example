c7652479c688716a5f9fa5dec04eafb6adfa12c3
Zach Etienne (RIT Student) <zce4200@rit.edu>
2026-01-28 12:50:50 -0500

    example done

--- src/responses.js
+++ src/responses.js
@@ -3,7 +3,10 @@ const fs = require('fs'); // pull in the file system module
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 
 const respond = (request, response, content, type) => {
-  response.writeHead(200, { 'Content-Type': type });
+  response.writeHead(200, { 
+    'Content-Type': type,
+    'Content-Length': Buffer.byteLength(content, 'utf8')
+   });
   response.write(content);
   response.end();
 };
@@ -11,6 +14,25 @@ const respond = (request, response, content, type) => {
 const getIndex = (request, response) => {
   respond(request, response, index, 'text/html');
 };
+
+const getCats = (request, response) => {
+  const cat = {
+    name: "cat",
+    age: 9,
+  }
+
+  if(request.acceptedTypes[0] === 'text/xml'){
+    let catXML = `<response>`;
+    catXML += `<name>${cat.name}</name>`;
+    catXML += `<age>${cat.age}</age>`;
+    catXML += `</response>`;
+
+  return respond(request, response, catXML, 'text/xml')
+  }
+
+  const catString = JSON.stringify(cat);
+  return respond(request, response, catString, 'application/json')
+}
 
 module.exports = {
   getCats,
--- src/server.js
+++ src/server.js
@@ -4,11 +4,19 @@ const responseHandler = require('./responses.js');
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const urlStruct = {
-
+  '/': responseHandler.getIndex,
+  '/cats': responseHandler.getCats,
+  index: responseHandler.getIndex
 };
 
 const onRequest = (request, response) => {
+  const protocol = request.connection.encrypted ? "https" : "http";
+  const parsedUrl = new URL(request.url, `${protocol}://${request.headers.host}`)
+  if(request.headers.accept) {request.acceptedTypes = request.headers.accept.split(',');}
 
+  const handler = urlStruct[parsedUrl.pathname];
+  if (handler) handler(request, response);
+  else urlStruct.index(request, response);
 };
 
 http.createServer(onRequest).listen(port, () => {
